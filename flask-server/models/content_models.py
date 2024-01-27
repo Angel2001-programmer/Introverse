31d11ff17d077b60c79e345052500fa70f244114
Meteorstrike <k.h.materials@gmail.com>
2024-01-27 15:44:38 +0000

    Added doc strings to table models

--- flask-server/models/content_models.py
+++ flask-server/models/content_models.py
@@ -2,11 +2,40 @@ import sys
 sys.path.append("..")
 from exts import db
 
-# db = SQLAlchemy()
 
-
-# Class for Books table
 class Books(db.Model):
+    """
+    Class model for the Books table.
+
+    ...
+
+    Attributes
+    ----------
+    book_id : int
+        id of the book
+    book_name : str
+        name of the book
+    book_author : str
+        author of the book
+    book_genre : str
+        genre of the book
+    price : float
+        rrp of the book
+    book_script : str (text)
+        description of the book
+    book_image : str
+        image url of the book
+
+    Methods
+    -------
+    __repr__
+        Returns a string representation of the constructed object.
+    create
+        Adds a new book object to the database.
+    delete
+        Deletes a book object from the database.
+    """
+
     __tablename__ = "Books"
     Book_ID = db.Column(db.Integer, primary_key=True, unique=True)
     Book_Name = db.Column(db.String(100), unique=True, nullable=False)
@@ -16,9 +45,52 @@ class Books(db.Model):
     Book_Script = db.Column(db.String(1000))
     Book_Image = db.Column(db.String(100), unique=True)
 
+    def __repr__(self):
+        """Returns a string representation of constructed object."""
+        return f"<Book title {self.Book_Name}, by author {self.Book_Author}.>"
+    
+    def create(self):
+        """Adds a new book object to the database."""
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        """Deletes a book object from the database."""
+        db.session.delete(self)
+        db.session.commit()
 
-# Class for Anime table
+
 class Anime(db.Model):
+    """
+    Class model for the Anime table.
+
+    ...
+
+    Attributes
+    ----------
+    anime_id : int
+        id of the anime
+    anime_name : str
+        name of the anime
+    anime_genre : str
+        genre of the anime
+    where_tw : str
+        where to watch the anime
+    anime_script : str (text)
+        description of the anime
+    anime_image : str
+        image url of the anime
+
+    Methods
+    -------
+    __repr__
+        Returns a string representation of the constructed object.
+    create
+        Adds a new anime object to the database.
+    delete
+        Deletes a anime object from the database.
+    """
+
     __tablename__ = "Anime"
     Anime_ID = db.Column(db.Integer, primary_key=True, unique=True)
     Anime_Name = db.Column(db.String(50), unique=True, nullable=False)
@@ -27,9 +99,54 @@ class Anime(db.Model):
     Anime_Script = db.Column(db.String(1000))
     Anime_Image = db.Column(db.String(100), unique=True)
 
+    def __repr__(self):
+        """Returns a string representation of constructed object."""
+        return f"<Anime {self.Anime_Name}>"
+    
+    def create(self):
+        """Adds a new anime object to the database."""
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        """Deletes an anime object from the database."""
+        db.session.delete(self)
+        db.session.commit()
 
-# Class for Games table
+
 class Games(db.Model):
+    """
+    Class model for the Games table.
+
+    ...
+
+    Attributes
+    ----------
+    game_id : int
+        id of the game
+    game_name : str
+        name of the game
+    game_genre : str
+        genre of the game
+    w_console : str
+        which consoles to play the game
+    price : float
+        rrp of the game
+    game_script : str (text)
+        description of the game
+    game_image : str
+        image url of the game
+
+    Methods
+    -------
+    __repr__
+        Returns a string representation of the constructed object.
+    create
+        Adds a new game object to the database.
+    delete
+        Deletes a game object from the database.
+    """
+
     __tablename__ = "Games"
     Game_ID = db.Column(db.Integer, primary_key=True, unique=True)
     Game_Name = db.Column(db.String(50), unique=True, nullable=False)
@@ -38,3 +155,17 @@ class Games(db.Model):
     Price = db.Column(db.Float, nullable=False)
     Game_Script = db.Column(db.String(1000))
     Game_Image = db.Column(db.String(100), unique=True)
+
+    def __repr__(self):
+        """Returns a string representation of constructed object."""
+        return f"<Game {self.Game_Name}>"
+    
+    def create(self):
+        """Adds a new game object to the database."""
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        """Deletes a game object from the database."""
+        db.session.delete(self)
+        db.session.commit()
--- flask-server/models/user_models.py
+++ flask-server/models/user_models.py
@@ -2,39 +2,120 @@ import sys
 sys.path.append("..")
 from exts import db
 from uuid import uuid4
-from datetime import datetime as dt  # To make a timestamp
+from datetime import datetime as dt
 
 
-# Generate a unique user ID, 32 characters long
 def get_uuid():
+    """Returns a unique user ID"""
     return uuid4().hex
 
 
-# Class for user account table, all the important information in here
 class User(db.Model):
+    """
+    Class model for the user accounts table.
+
+    ...
+
+    Attributes
+    ----------
+    user_id : str
+        unique identity code for the user, generated from get_uuid function
+    username : str
+        username of the user
+    password : str
+        password of the user, stored as a bcrypt hash
+
+    Methods
+    -------
+    __repr__
+        Returns a string representation of the constructed object.
+    """
+
     __tablename__ = "user_accounts"
-    user_id = db.Column(db.String(36), primary_key=True, unique=True, default=get_uuid)  # If users are inserted through MySQL the UUID will be 36 chars
+    user_id = db.Column(db.String(36), primary_key=True, unique=True, default=get_uuid)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)  # Bcrypt should be 60 chars
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        """Returns a string representation of constructed object."""
+        return f"<User {self.username}>"
 
 
-# Class for user profile table, columns by default to be null and added by user if they wish on edit profile page
 class Profile(db.Model):
+    """
+    Class model for the user profiles table.
+
+    ...
+
+    Attributes
+    ----------
+    username : str
+        username of the user
+    first_name : str
+        first name of the user
+    last_name : str
+        last name of the user
+    email : str
+        email address of the user
+    date_of_birth : date
+        date of birth of the user
+    interests : str (text)
+        interests of the user
+    date_joined : datetime
+        timestamp of sign up for the user
+
+    Methods
+    -------
+    __repr__
+        Returns a string representation of the constructed object.
+    """
+
     __tablename__ = "user_profiles"
-    username = db.Column(db.String(30), primary_key=True, unique=True)  # Changing from user id to username
+    username = db.Column(db.String(30), primary_key=True, unique=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(254), unique=True, nullable=False)
-    date_of_birth = db.Column(db.Date)  # This option for future functionality of calculating age and age restricting recommendations -> if null restricted by default
+    date_of_birth = db.Column(db.Date)  # This option for future functionality of calculating age and age restricting recommendations -> change this to not null when get working
     interests = db.Column(db.Text)
     date_joined = db.Column(db.DateTime(), default=dt.utcnow)
 
+    def __repr__(self):
+        """Returns a string representation of constructed object."""
+        return f"<User {self.username}>"
+
 
 class Message(db.Model):
+    """
+    Class model for the message board table.
+
+    ...
+
+    Attributes
+    ----------
+    post_id : int
+        id of the message
+    post_content : str (text)
+        text content of the message
+    post_category : str
+        category of the message
+    post_author : str
+        author of the message
+    post_date : datetime
+        time and date that the message was posted
+
+    Methods
+    -------
+    __repr__
+        Returns a string representation of the constructed object.
+    """
+
     __tablename__ = "message_board"
     post_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     post_content = db.Column(db.Text, nullable=False)
     post_category = db.Column(db.String(50), nullable=False)
-    post_author = db.Column(db.String(30), nullable=False)
+    post_author = db.Column(db.String(30), nullable=False)  # Change back to being a FK at some point
     post_date = db.Column(db.DateTime(), default=dt.utcnow, nullable=False)
-    # post_author = db.Column(db.String(30), db.ForeignKey(User.username), nullable=False)  # Dropping the FK restraint for simplicity of demonstrating messages from mock users
+
+    def __repr__(self):
+        """Returns a string representation of constructed object."""
+        return f"<Message ID {self.post_id}, by user {self.post_author}, posted at {self.post_date}.>"
