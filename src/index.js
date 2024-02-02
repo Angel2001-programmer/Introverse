098f0862407d50ec949771da5863ffba44a976ab
Meteorstrike <k.h.materials@gmail.com>
2024-02-02 13:23:13 +0000

    Persist added to redux

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from "./redux/store";
-import { Provider } from 'react-redux'; 
+import { Provider } from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from 'redux-persist';
 import reportWebVitals from './reportWebVitals';
 
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
