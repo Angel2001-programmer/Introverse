c969f54fd65365c8177c3ed3053eb5b3d1a92f3f
Angel2001-programmer <angelwitchell2001@gmail.com>
2024-02-15 16:23:59 +0000

    Enabled db to work on my system and linked up backend data on the UI. fixed date and time format on editPosts.jsx

--- src/components/EditPosts/EditPosts.jsx
+++ src/components/EditPosts/EditPosts.jsx
@@ -1,37 +1,55 @@
-import styles from "./EditPosts.module.css";
+import styles from './EditPosts.module.css';
 import profile from '../../assets/images/logos/user.png';
 
-const EditPosts = props => {
-    return (
-        <div className={styles.postsContainer}>
-					<h2>Posts</h2>
-					{props.data.map((post) => (
-						<div key={post.post_id} className={styles.post}>
-							<div className={styles.postProfilecontainer}>
-								<div className={styles.userPhotoContainer}>
-									<img src={profile} alt='profile.' />
-								</div>
-								<div className={styles.userInfocontainer}>
-									<h6>{post.post_author}</h6>
-									<h6>{post.post_category}</h6>
-								</div>
-							</div>
+const EditPosts = (props) => {
+  return (
+    <div className={styles.postsContainer}>
+      <h2>Posts</h2>
+      {props.data.map((post) => (
+        <div
+          key={post.post_id}
+          className={styles.post}
+        >
+          <div className={styles.postProfilecontainer}>
+            <div className={styles.userPhotoContainer}>
+              <img
+                src={profile}
+                alt='profile.'
+              />
+            </div>
+            <div className={styles.userInfocontainer}>
+              <h6>{post.post_author}</h6>
+              <h6>{post.post_category}</h6>
+            </div>
+          </div>
 
-							<p className={styles.postContent}>
-								{post.post_content}
-							</p>
+          <p className={styles.postContent}>{post.post_content}</p>
 
-							<p>
-								{post.post_date}
-							</p>
-							<div className={styles.commentContainer}>
-            				<img className={styles.comment} src={require("../../assets/icons/comments.png")} alt="comments"/>
-            				<p>0</p>
-        					</div> 
-						</div>
-					))}
-				</div>
-    )
-}
+          <p>
+            {new Date(post.post_date).toLocaleDateString('en-GB', {
+              weekday: 'short',
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric',
+            })}
+          </p>
+          <p style={{ margin: '0px', padding: '0px' }}>
+            {new Date(post.post_date).toLocaleTimeString('en-GB', {
+              hour12: 'true',
+            })}
+          </p>
+          <div className={styles.commentContainer}>
+            <img
+              className={styles.comment}
+              src={require('../../assets/icons/comments.png')}
+              alt='comments'
+            />
+            <p>0</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default EditPosts;
--- src/pages/editAccount/editAccount.jsx
+++ src/pages/editAccount/editAccount.jsx
@@ -3,28 +3,28 @@ import styles from './editAccount.module.css';
 import NavBar from '../../components/NavBar/navbar';
 import DropDownMenu from '../../components/DropDownMenu/dropDownMenu';
 import MobileNav from '../../components/MobileNav/MobileNav';
-import EditPosts from "../../components/EditPosts/EditPosts";
-import EditBanner from "../../components/EditBanner/EditBanner";
-import EditDetailsProfile from "../../components/EditProfileDetails/EditProfileDetails";
-import httpClient from "../../httpClient";
-import { UserNameContext } from "../../components/FinalProject/FinalProject";
-import { useSelector } from "react-redux"
-import { selectCurrentUser } from "../../redux/slices/userSlice"
+import EditPosts from '../../components/EditPosts/EditPosts';
+import EditBanner from '../../components/EditBanner/EditBanner';
+import EditDetailsProfile from '../../components/EditProfileDetails/EditProfileDetails';
+import httpClient from '../../httpClient';
+import { UserNameContext } from '../../components/FinalProject/FinalProject';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../redux/slices/userSlice';
 
 const EditAccount = () => {
-	const [isPressed, setIsPressed] = useState(false);
-	const [posts, setPosts] = useState(null);
-	const [filteredposts, setfilteredposts] = useState(null);
+  const [isPressed, setIsPressed] = useState(false);
+  const [posts, setPosts] = useState(null);
+  const [filteredposts, setfilteredposts] = useState(null);
 
   // Think we can replace the useContext with the useSelector
-	const [UserName, setUserName] = useContext(UserNameContext);
+  const [UserName, setUserName] = useContext(UserNameContext);
 
-	const user = useSelector(selectCurrentUser)
+  const user = useSelector(selectCurrentUser);
 
-	console.log(UserName)
+  console.log(UserName);
 
-	let error = null;
-	let filteredList = null;
+  let error = null;
+  let filteredList = null;
 
   /* 
   To do
@@ -34,45 +34,56 @@ const EditAccount = () => {
   */
 
   useEffect(() => {
-  const getForms = async () => {
-    const res = await httpClient.get("http://localhost:5000/forum/all")
-    .then(res => { 
-      setPosts(res.data)    
-      filteredList = res.data.filter((list) => list.post_author === user.name)
-        console.log(filteredList);
-      setfilteredposts(filteredList);
-      if(filteredList.length === 0){
-      error = <h2>No Posts Yet.</h2>
-      setPosts(null)
-      }
-    })
-    .catch(err => {
-      setPosts(null);
-      console.log(err);
-      error = <h2>No Posts Yet.</h2>});
-  }
-  getForms();
-  }, [])
+    const getForms = async () => {
+      const res = await httpClient
+        .get('http://localhost:5000/forum/all')
+        .then((res) => {
+          setPosts(res.data);
+          filteredList = res.data.filter(
+            (list) => list.post_author === user.name
+          );
+          console.log(filteredList);
+          setfilteredposts(filteredList);
+          if (filteredList.length === 0) {
+            error = <h2>No Posts Yet.</h2>;
+            setPosts(null);
+          }
+        })
+        .catch((err) => {
+          setPosts(null);
+          console.log(err);
+          error = <h2>No Posts Yet.</h2>;
+        });
+    };
+    getForms();
+  }, []);
 
+  console.log(filteredList);
 
-	return (
-		<Fragment>
-			<NavBar isPressed={isPressed} onChangePressed={setIsPressed} />
-			<MobileNav/>
-			<DropDownMenu isPressed={isPressed} setIsPressed={setIsPressed} />
-			<main className={styles.main}>
-				<EditBanner/>
-				{posts !== null?
-				<EditPosts data={filteredposts}/>
-				:
-				<div className={styles.NoPosts}>
-				<h2>No Posts Yet.</h2>				
-				</div>
-				}
-				<EditDetailsProfile/>
-			</main>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <NavBar
+        isPressed={isPressed}
+        onChangePressed={setIsPressed}
+      />
+      <MobileNav />
+      <DropDownMenu
+        isPressed={isPressed}
+        setIsPressed={setIsPressed}
+      />
+      <main className={styles.main}>
+        <EditBanner />
+        {posts !== null ? (
+          <EditPosts data={filteredposts} />
+        ) : (
+          <div className={styles.NoPosts}>
+            <h2>No Posts Yet.</h2>
+          </div>
+        )}
+        <EditDetailsProfile />
+      </main>
+    </Fragment>
+  );
 };
 
 export default EditAccount;
